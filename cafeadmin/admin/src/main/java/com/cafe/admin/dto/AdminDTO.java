16d79adb40901c203879a61de7998c0bf77b9ab2
charcoal <devsj3586@gmail.com>
2022-12-26 20:24:56 +0900

    2022.12.26 example setting

---
+++ cafeadmin/admin/src/main/java/com/cafe/admin/dto/AdminDTO.java
@@ -0,0 +1,15 @@
+package com.cafe.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AdminDTO {
+    private String id;
+    private String password;
+}
